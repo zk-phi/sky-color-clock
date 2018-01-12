664b38d88051e9ba695cd1903326ba61a2c333a0
p <_>
2018-01-12 18:56:34 +0900

    Revert "Slightly weaken temperature indicator"
    This reverts commit d0643203dc3bf30dd12ce61f6631652a60595e19.

--- sky-color-clock.el
+++ sky-color-clock.el
@@ -171,8 +171,7 @@ saturate according to CLOUDINESS. CLOUDINESS can be a number from
   (if (null temperature) ""
     (let ((color (sky-color-clock--blend-colors
                   basecolor
-                  (funcall sky-color-clock--temperature-color-gradient temperature)
-                  0.4)))
+                  (funcall sky-color-clock--temperature-color-gradient temperature))))
       (propertize " " 'face `(:background ,color)))))
 
 ;; ---- emoji moonphase
